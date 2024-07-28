7821c505aa82fee30a6cb519f043a8ad176c293b
Halil Aydın <84444757+AydinTheFirst@users.noreply.github.com>
2024-07-28 14:42:06 +0300

    first commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/no-unescaped-entities */
+const Home = () => {
+  return (
+    <div className="container">
+      <div className="navbar">x</div>
+    </div>
+  );
+};
+
+export default Home;
